b318593236c64cff27e3fe4da9392d00d73a172a
Steve Purcell <steve@sanityinc.com>
2019-09-13 09:25:25 +1200

    Prefer dashes to undercores in attribute names

--- default.nix
+++ default.nix
@@ -16,35 +16,35 @@ let
 in
 {
   # TODO: These early versions fail for me at the bootstrap phase on MacOS
-  #emacs_24_1 = old-emacs-pkgs.callPackage ./emacs.nix { version = "24.1"; sha256 = "1awbgkwinpqpzcn841kaw5cszdn8sx6jyfp879a5bff0v78nvlk0"; };
-  #emacs_24_2 = old-emacs-pkgs.callPackage ./emacs.nix { version = "24.2"; sha256 = "0mykbg5rzrm2h4805y4nl5vpvwx4xcmp285sbr51sxp1yvgr563d"; withAutoReconf = false; };
+  #emacs24-1 = old-emacs-pkgs.callPackage ./emacs.nix { version = "24.1"; sha256 = "1awbgkwinpqpzcn841kaw5cszdn8sx6jyfp879a5bff0v78nvlk0"; };
+  #emacs24-2 = old-emacs-pkgs.callPackage ./emacs.nix { version = "24.2"; sha256 = "0mykbg5rzrm2h4805y4nl5vpvwx4xcmp285sbr51sxp1yvgr563d"; withAutoReconf = false; };
 
-  emacs_24_3 = with old-emacs-pkgs; callPackage ./emacs.nix {
+  emacs24-3 = with old-emacs-pkgs; callPackage ./emacs.nix {
     version = "24.3";
     sha256 = "0hggksbn9h5gxmmzbgzlc8hgl0c77simn10jhk6njgc10hrcm600";
     withAutoReconf = false;
     stdenv = clangStdenv;
   };
 
-  emacs_24_4 = with pkgs; callPackage ./emacs.nix {
+  emacs24-4 = with pkgs; callPackage ./emacs.nix {
     version = "24.4";
     sha256 = "1iicqcijr56r7vxxm3v3qhf69xpxlpq7afbjr6h6bpjsz8d4yg59";
     withAutoReconf = false;
     stdenv = clangStdenv;
   };
 
-  emacs_24_5 = with pkgs; callPackage ./emacs.nix {
+  emacs24-5 = with pkgs; callPackage ./emacs.nix {
     version = "24.5";
     sha256 = "1dn3jx1dph5wr47v97g0fhka9gcpn8pnzys7khp9indj5xiacdr7";
     withAutoReconf = false;
     stdenv = clangStdenv;
   };
 
-  emacs_25_1 = pkgs.callPackage ./emacs.nix { version = "25.1"; sha256 = "0rqw9ama0j5b6l4czqj4wlf21gcxi9s18p8cx6ghxm5l1nwl8cvn"; withAutoReconf = true; };
-  emacs_25_2 = pkgs.callPackage ./emacs.nix { version = "25.2"; sha256 = "0b9dwx6nxzflaipkgml4snny2c3brgy0py6h05q995y1lrpbsnsh"; withAutoReconf = true; };
-  emacs_25_3 = pkgs.callPackage ./emacs.nix { version = "25.3"; sha256 = "1jc3g79nrcix0500kiw6hqpql82ajq0xivlip6iaryxn90dnlb7p"; withAutoReconf = true; };
-  emacs_26_1 = pkgs.callPackage ./emacs.nix { version = "26.1"; sha256 = "18vaqn7y7c39as4bn95yfcabwvqkw6y59xz8g78d1ifdx3aq40vn"; withAutoReconf = true; };
-  emacs_26_2 = pkgs.callPackage ./emacs.nix { version = "26.2"; sha256 = "1sxl0bqwl9b62nswxaiqh1xa61f3hng4fmyc69lmadx770mfb6ag"; withAutoReconf = true; };
-  emacs_26_3 = pkgs.callPackage ./emacs.nix { version = "26.3"; sha256 = "14bm73758w6ydxlvckfy9nby015p20lh2yvl6pnrjz0k93h4giq9"; withAutoReconf = true; };
+  emacs25-1 = pkgs.callPackage ./emacs.nix { version = "25.1"; sha256 = "0rqw9ama0j5b6l4czqj4wlf21gcxi9s18p8cx6ghxm5l1nwl8cvn"; withAutoReconf = true; };
+  emacs25-2 = pkgs.callPackage ./emacs.nix { version = "25.2"; sha256 = "0b9dwx6nxzflaipkgml4snny2c3brgy0py6h05q995y1lrpbsnsh"; withAutoReconf = true; };
+  emacs25-3 = pkgs.callPackage ./emacs.nix { version = "25.3"; sha256 = "1jc3g79nrcix0500kiw6hqpql82ajq0xivlip6iaryxn90dnlb7p"; withAutoReconf = true; };
+  emacs26-1 = pkgs.callPackage ./emacs.nix { version = "26.1"; sha256 = "18vaqn7y7c39as4bn95yfcabwvqkw6y59xz8g78d1ifdx3aq40vn"; withAutoReconf = true; };
+  emacs26-2 = pkgs.callPackage ./emacs.nix { version = "26.2"; sha256 = "1sxl0bqwl9b62nswxaiqh1xa61f3hng4fmyc69lmadx770mfb6ag"; withAutoReconf = true; };
+  emacs26-3 = pkgs.callPackage ./emacs.nix { version = "26.3"; sha256 = "14bm73758w6ydxlvckfy9nby015p20lh2yvl6pnrjz0k93h4giq9"; withAutoReconf = true; };
   # TODO: HEAD
 }
