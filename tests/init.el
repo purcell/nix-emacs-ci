a90c7f20a281266cbe745c0dda82387303019218
Steve Purcell <steve@sanityinc.com>
2019-09-23 14:48:13 +1200

    Install packages as part of startup smoke test

--- tests/init.el
+++ tests/init.el
@@ -1,11 +1,26 @@
+(setq custom-file (make-temp-file "custom" nil ".el"))
+
 ;; Check we can have GNUTLS support
 (unless (gnutls-available-p)
- (error "GnuTLS appears unavailable"))
+  (error "GnuTLS appears unavailable"))
 
 (message "Testing we can talk https to GNU")
 (with-temp-buffer
- (url-insert-file-contents "https://elpa.gnu.org/"))
+  (url-insert-file-contents "https://elpa.gnu.org/"))
 
 (message "Testing we can talk https to MELPA")
 (with-temp-buffer
- (url-insert-file-contents "http://melpa.org/"))
+  (url-insert-file-contents "https://melpa.org/"))
+
+(require 'package)
+(push '("melpa" . "https://melpa.org/packages/") package-archives)
+
+(message "Checking we can fetch package archives")
+(package-refresh-contents)
+(package-initialize)
+
+(message "Testing we can install from ELPA")
+(package-install 'all)
+
+(message "Testing we can install from MELPA")
+(package-install 'unfill)
