6c0498e8b656c350418a77168d7b1652aecde390
Akira Komamura <akira.komamura@gmail.com>
2021-09-26 11:55:18 +0900

    Add pre-commit check

--- flake.nix
+++ flake.nix
@@ -2,8 +2,13 @@
   description = "Emacs installations for continuous integration";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pre-commit-hooks = {
+    url = "github:cachix/pre-commit-hooks.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -20,6 +25,18 @@
         defaultPackage = packages.emacs-27-2;
         lib = {
           inherit (attrs) emacs-ci-versions;
+        };
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              nix-linter.enable = true;
+            };
+          };
+        };
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
         };
       }
     );
