c9e565b15f0632d64d064944d852d896b4fabee3
Akira Komamura <akira.komamura@gmail.com>
2021-09-26 11:19:37 +0900

    Provide the same derivations as flake.nix from default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,14 @@
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { overlays = [ (import ./overlay.nix) ]; };
+  system = builtins.currentSystem;
 in
-builtins.listToAttrs
-  (builtins.map (a: { name = a; value = builtins.getAttr a pkgs; })
-    pkgs.emacs-ci-versions)
+(import
+  (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }
+  )
+  {
+    inherit system;
+    src = ./.;
+  }).defaultNix.packages.${system}
