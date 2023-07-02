e23512ce4d421ec55982dda92e2e60f29be55f4f
Steve Purcell <steve@sanityinc.com>
2023-07-02 15:15:36 +0200

    Fix up tests/default.nix to use local flake

--- tests/default.nix
+++ tests/default.nix
@@ -1,28 +1,27 @@
-{emacsAttr ? null}: let
-  pkgs = import (import ../nix/sources.nix).nixpkgs {
-    overlays = [(import ../overlay.nix)];
-  };
+{ emacsAttr ? null }:
+let
+  flake = builtins.getFlake (toString ../.);
+  pkgs = import flake.inputs.nixpkgs { };
   emacs =
     if emacsAttr == null
     then pkgs.emacs
-    else (import ../default.nix).${emacsAttr};
+    else flake.packages.${builtins.currentSystem}.${emacsAttr};
 in
-  pkgs.writeShellApplication {
-    name = "test";
-    runtimeInputs = [
-      ((pkgs.emacs.pkgs.overrideScope' (_: _: {
-          inherit emacs;
-        }))
-        .withPackages (
-          epkgs: [
-            epkgs.seq
-            epkgs.dash
-          ]
-        ))
-    ];
-    text = ''
-      emacs --version
-      emacs -batch -q -l seq -l dash
-      echo "Successfully loaded."
-    '';
-  }
+pkgs.writeShellApplication {
+  name = "test";
+  runtimeInputs = [
+    ((pkgs.emacs.pkgs.overrideScope' (_: _: {
+      inherit emacs;
+    })).withPackages (
+      epkgs: [
+        epkgs.seq
+        epkgs.dash
+      ]
+    ))
+  ];
+  text = ''
+    emacs --version
+    emacs -batch -q -l seq -l dash
+    echo "Successfully loaded."
+  '';
+}
