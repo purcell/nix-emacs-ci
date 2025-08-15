8a60924876aba9d8b52d0dc6fa4f8e91364db943
Steve Purcell <steve@sanityinc.com>
2025-08-15 09:42:16 +0100

    Run nixfmt

--- flake.nix
+++ flake.nix
@@ -105,82 +105,115 @@
 
   nixConfig = {
     extra-substituters = "https://emacs-ci.cachix.org";
-    extra-trusted-public-keys =
-      "emacs-ci.cachix.org-1:B5FVOrxhXXrOL0S+tQ7USrhjMT5iOPH+QN9q0NItom4=";
+    extra-trusted-public-keys = "emacs-ci.cachix.org-1:B5FVOrxhXXrOL0S+tQ7USrhjMT5iOPH+QN9q0NItom4=";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: {
-    packages = nixpkgs.lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ] (system:
-      let
-        inherit (nixpkgs) lib;
-        pkgs = nixpkgs.legacyPackages.${system};
-        versions =
-          # Some versions do not currently build on MacOS, so we do not even
-          # expose them on that platform.
-          (lib.optionalAttrs pkgs.stdenv.isLinux {
-            emacs-23-4 = "23.4";
-            emacs-24-1 = "24.1";
-            emacs-24-2 = "24.2";
-          }) // (lib.optionalAttrs (system != "aarch64-darwin") {
-            emacs-24-3 = "24.3";
-            emacs-24-4 = "24.4";
-            emacs-24-5 = "24.5";
-            emacs-25-1 = "25.1";
-            emacs-25-2 = "25.2";
-            emacs-25-3 = "25.3";
-            emacs-26-1 = "26.1";
-            emacs-26-2 = "26.2";
-            emacs-26-3 = "26.3";
-            emacs-27-1 = "27.1";
-            emacs-27-2 = "27.2";
-          }) // {
-            emacs-28-1 = "28.1";
-            emacs-28-2 = "28.2";
-            emacs-29-1 = "29.1";
-            emacs-29-2 = "29.2";
-            emacs-29-3 = "29.3";
-            emacs-29-4 = "29.4";
-            emacs-30-1 = "30.1";
-            emacs-release-snapshot = "30.1.50";
-            emacs-snapshot = "31.0.50";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    {
+      packages =
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-darwin"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            let
+              inherit (nixpkgs) lib;
+              pkgs = nixpkgs.legacyPackages.${system};
+              versions =
+                # Some versions do not currently build on MacOS, so we do not even
+                # expose them on that platform.
+                (lib.optionalAttrs pkgs.stdenv.isLinux {
+                  emacs-23-4 = "23.4";
+                  emacs-24-1 = "24.1";
+                  emacs-24-2 = "24.2";
+                })
+                // (lib.optionalAttrs (system != "aarch64-darwin") {
+                  emacs-24-3 = "24.3";
+                  emacs-24-4 = "24.4";
+                  emacs-24-5 = "24.5";
+                  emacs-25-1 = "25.1";
+                  emacs-25-2 = "25.2";
+                  emacs-25-3 = "25.3";
+                  emacs-26-1 = "26.1";
+                  emacs-26-2 = "26.2";
+                  emacs-26-3 = "26.3";
+                  emacs-27-1 = "27.1";
+                  emacs-27-2 = "27.2";
+                })
+                // {
+                  emacs-28-1 = "28.1";
+                  emacs-28-2 = "28.2";
+                  emacs-29-1 = "29.1";
+                  emacs-29-2 = "29.2";
+                  emacs-29-3 = "29.3";
+                  emacs-29-4 = "29.4";
+                  emacs-30-1 = "30.1";
+                  emacs-release-snapshot = "30.1.50";
+                  emacs-snapshot = "31.0.50";
+                };
+            in
+            builtins.mapAttrs (
+              name: version:
+              pkgs.callPackage ./emacs.nix {
+                inherit name version;
+                inherit (pkgs.darwin) sigtool;
+                src = inputs.${name};
+                latestPackageKeyring = inputs.emacs-snapshot + "/etc/package-keyring.gpg";
+                srcRepo = lib.strings.hasInfix "snapshot" version;
+              }
+            ) versions
+          );
+
+      checks = builtins.mapAttrs (
+        system:
+        builtins.mapAttrs (
+          _name: ciEmacs:
+          nixpkgs.legacyPackages.${system}.callPackage ./tests {
+            inherit ciEmacs;
+          }
+        )
+      ) self.packages;
+
+      githubActions =
+        let
+          inherit (builtins)
+            attrValues
+            mapAttrs
+            attrNames
+            map
+            concatLists
+            intersectAttrs
+            ;
+          platforms = {
+            "x86_64-linux" = "ubuntu-latest";
+            #"x86_64-darwin" = "macos-latest";
+            "aarch64-darwin" = "macos-latest";
           };
-      in builtins.mapAttrs (name: version:
-        pkgs.callPackage ./emacs.nix {
-          inherit name version;
-          inherit (pkgs.darwin) sigtool;
-          src = inputs.${name};
-          latestPackageKeyring = inputs.emacs-snapshot
-            + "/etc/package-keyring.gpg";
-          srcRepo = lib.strings.hasInfix "snapshot" version;
-        }) versions);
-
-    checks = builtins.mapAttrs (system:
-      builtins.mapAttrs (_name: ciEmacs:
-        nixpkgs.legacyPackages.${system}.callPackage ./tests {
-          inherit ciEmacs;
-        })) self.packages;
-
-    githubActions = let
-      inherit (builtins)
-        attrValues mapAttrs attrNames map concatLists intersectAttrs;
-      platforms = {
-        "x86_64-linux" = "ubuntu-latest";
-        #"x86_64-darwin" = "macos-latest";
-        "aarch64-darwin" = "macos-latest";
-      };
-    in rec {
-      checks = intersectAttrs platforms self.checks;
-      matrix.include = concatLists (attrValues (mapAttrs (system: pkgs:
-        map (pkg: {
-          inherit system;
-          attr = pkg;
-          os = platforms.${system};
-        }) (attrNames pkgs)) checks));
+        in
+        rec {
+          checks = intersectAttrs platforms self.checks;
+          matrix.include = concatLists (
+            attrValues (
+              mapAttrs (
+                system: pkgs:
+                map (pkg: {
+                  inherit system;
+                  attr = pkg;
+                  os = platforms.${system};
+                }) (attrNames pkgs)
+              ) checks
+            )
+          );
+        };
     };
-  };
 }
