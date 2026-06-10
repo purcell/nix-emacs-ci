7752f86158953e603c7b781e789346b9647a35a1
Akira Komamura <akira.komamura@gmail.com>
2026-06-10 20:18:58 +0900

    Bump emacs-release-snapshot to 31.0.90

--- flake.nix
+++ flake.nix
@@ -164,7 +164,7 @@
                   emacs-29-4 = "29.4";
                   emacs-30-1 = "30.1";
                   emacs-30-2 = "30.2";
-                  emacs-release-snapshot = "31.0.60";
+                  emacs-release-snapshot = "31.0.90";
                   emacs-snapshot = "32.0.50";
                 };
             in
